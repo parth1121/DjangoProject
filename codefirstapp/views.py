fb23367d34d9298962fb0d2adbbaddd250900a94
parth <parthdarak11@gmail.com>
2021-08-10 09:11:03 +0530

    10-08

--- codefirstapp/views.py
+++ codefirstapp/views.py
@@ -3,5 +3,10 @@ from django.shortcuts import render
 
 
 # Create your views here.
+from codefirstapp.models import Product
+
+
 def test(request):
-    return render(request, 'index.html')
+    # Product.objects.create(product_name='Wheel Chair', product_desc='used for accessbvility patient')
+    product_list =Product.objects.all()
+    return render(request, 'index.html',{'product_list':product_list})
