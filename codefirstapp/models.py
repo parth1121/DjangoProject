35327f2cd2a06cffdda231cfd7b68d703a86e99f
parth <parthdarak11@gmail.com>
2021-08-10 07:43:48 +0530

    09-08

--- codefirstapp/models.py
+++ codefirstapp/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Product(models.Model):
+    product_name = models.CharField(max_length=50)
+    product_desc = models.CharField(max_length=2000)
+
+class ProductTest(models.Model):
+    product_name = models.CharField(max_length=50)
+    product_desc = models.CharField(max_length=2000)
--- codetest/settings.py
+++ codetest/settings.py
@@ -77,8 +77,10 @@ WSGI_APPLICATION = 'codetest.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'ngoproject',
+        'USER': 'root',
+        'PASSWORD': 'password',
     }
 }
 
