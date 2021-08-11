e89005f40b6fcafca5e6ba975db6f4da4a48ce8e
parth <parthdarak11@gmail.com>
2021-08-11 07:58:53 +0530

    10/8/21

--- codefirstapp/models.py
+++ codefirstapp/models.py
@@ -5,6 +5,7 @@ class Product(models.Model):
     product_name = models.CharField(max_length=50)
     product_desc = models.CharField(max_length=2000)
 
+
 class ProductTest(models.Model):
     product_name = models.CharField(max_length=50)
     product_desc = models.CharField(max_length=2000)
